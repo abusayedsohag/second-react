b7bc6ba102440176a04769e619dc032c4b3916c1
abusayedsohag <abusayedsohagislam25@gmail.com>
2024-10-23 03:25:03 +0600

    Flags Name and Flags Add in top

--- src/component/countries/Countris.jsx
+++ src/component/countries/Countris.jsx
@@ -3,9 +3,23 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../country/Country";
+import './countries.css';
 
 const Countris = () => {
     const [countries, setCountries] = useState([]);
+    const [VisitedCountries, setVisitedCountries] = useState([]);
+    const [VisitedFlags, setVisitedFlags] = useState([]);
+
+    const handleVisitedCountries = country => {
+        const newVisitedCountries = [...VisitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = flags => {
+        const newVisitedFlags = [...VisitedFlags, flags];
+        setVisitedFlags(newVisitedFlags);
+    }
+    
 
     // Load API data
     useEffect(() => {
@@ -16,10 +30,25 @@ const Countris = () => {
 
     return (
         <div>
-            <h1>Countries : {countries.length}</h1>
-            {
-                countries.map(country => <Country key={country.ccs2} country={country}></Country>)
-            }
+            <div>
+                <h4>Countries : {countries.length}</h4>
+                <p>Visited Countries : {VisitedCountries.length}</p>
+                <ul>
+                    {
+                        VisitedCountries.map(country => <li>{country.name.common}</li>)
+                    }
+                </ul>
+                <div className="flags-container">
+                    {
+                        VisitedFlags.map(flag => <img src={flag.flags.png}></img>)
+                    }
+                </div>
+            </div>
+            <div className="countries">
+                {
+                    countries.map(country => <Country key={country.ccs2} handleVisitedFlags={handleVisitedFlags} handleVisitedCountries={handleVisitedCountries} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,12 +1,31 @@
 // type rsc for shortcut structure
+import { useState } from 'react';
 import './country.css';
 
-const Country = ({country}) => {
-    const {name, flags} = country;
+const Country = ({country, handleVisitedCountries, handleVisitedFlags}) => {
+    const {name, flags, population, area, cca3} = country;
+
+    const [Toggle, setToggle] = useState(false);
+
+    const handleToggle = () => {
+        setToggle(!Toggle);
+    }
+
+    const handleButton = () => {
+        handleVisitedCountries(country);
+        handleToggle();
+    }
+
     return (
-        <div className='box'>
+        <div className={`box ${Toggle && 'visited'}`}>
             <img src={flags.png} className='flag'/>
-            <h2>{name?.common} </h2>
+            <h2 className={`${Toggle && 'textColor'}`}>{name?.common} </h2>
+            <h5 className={`${Toggle && 'textColor'}`}>Population : {population}</h5>
+            <h5 className={`${Toggle && 'textColor'}`}>Area : {area}</h5>
+            <h5 className={`${Toggle && 'textColor'}`}>Code : {cca3}</h5>
+            <button onClick={handleButton}>{Toggle ? 'Visited' : 'Visite Here'}</button>
+            <button onClick={() => handleVisitedFlags({flags})}>Flag Add</button>
+            {Toggle ? 'Visiting': 'Visite Done'}
         </div>
     );
 };
